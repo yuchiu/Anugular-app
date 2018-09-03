6f06a6a4cbc138f04d5f541123e65493a1ef56f5
yuchiu <yuchiu2002@hotmail.com>
2018-09-03 12:58:10 -0400

    init proj with ng cli

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,22 @@
+import { NgModule, SkipSelf, Optional } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [HeaderComponent, FooterComponent, SidebarComponent],
+  exports: [HeaderComponent, FooterComponent, SidebarComponent]
+})
+export class CoreModule {
+  constructor(
+    @Optional()
+    @SkipSelf()
+    parent: CoreModule
+  ) {
+    if (parent) {
+      throw new Error("module already exist, can not reload again");
+    }
+  }
+}

5d5fdd731ddd4980444b05c41f48fc8efe1630fc
yuchiu <yuchiu2002@hotmail.com>
2018-09-03 13:25:54 -0400

    setup layout of header main footer and nav, added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { MdSidenavModule } from "@angular/material";
 
 import { CoreModule } from "./core/core.module";
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule],
+  imports: [BrowserModule, CoreModule, MdSidenavModule],
   providers: [],
   bootstrap: [AppComponent]
 })

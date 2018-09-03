5810ad3b0ee9746a367a23c8d7b665df6b386eeb
yuchiu <yuchiu2002@hotmail.com>
2018-09-03 19:04:13 -0400

    setup layout with angular material

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,12 @@
 import { NgModule, SkipSelf, Optional } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { MdToolbarModule } from "@angular/material";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { SidebarComponent } from "./sidebar/sidebar.component";
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MdToolbarModule],
   declarations: [HeaderComponent, FooterComponent, SidebarComponent],
   exports: [HeaderComponent, FooterComponent, SidebarComponent]
 })
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
+  selector: "app-header",
+  templateUrl: "./header.component.html",
   styles: []
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Output() toggle = new EventEmitter<void>();
+  ngOnInit() {}
 
-  ngOnInit() {
+  toggleSidebar() {
+    this.toggle.emit();
   }
-
 }
